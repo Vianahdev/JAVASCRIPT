586caab383e3eeeca3afad83fc57113bd8de3085
Vianah-dev <67674592+Vianah-dev@users.noreply.github.com>
2020-08-19 23:14:33 -0300

    commit

---
+++ Aula 10/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60.5
+console.log(`a velocidade do seu carro e de ${vel} Km/h`);
+
+if(vel > 60.5){
+    console.log(`Voce ultrapassou o limite permitido da via, Multado!`);
+}
---
+++ Aula 10/ex009.js
@@ -0,0 +1,10 @@
+var pais = 'Brasil'
+var r
+
+if (pais == 'Brasil'){
+    r='Brasileiro'
+}else{
+    r='Estrangeiro'
+}
+
+console.log(`voce é ${r}`)
