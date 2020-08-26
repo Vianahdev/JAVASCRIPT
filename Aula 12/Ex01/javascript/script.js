95f09fe9051ca7367673c0845bedbb9f758f8df5
Vianah-dev <67674592+Vianah-dev@users.noreply.github.com>
2020-08-26 20:56:07 -0300

    ok

--- Aula 12/Ex01/javascript/script.js
+++ Aula 12/Ex01/javascript/script.js
@@ -1,8 +1,25 @@
+function carregar(){
+
+var msg = window.document.getElementById('msg')
+var img = window.document.getElementById('imagem')
+
 var periodo = new Date()
-var hora = periodo.getHours();
+//var hora = periodo.getHours()
+var min = periodo.getMinutes()
 
-console.log(`${hora}`)
+hora = 19
 
-if(hora <= 12){
+msg.innerHTML = `Agora São exatamente ${hora}H ${min}Min `
+
+if(hora <=12){
+    img.src = 'imagem/dia.jpg'
+    document.body.style.background = '#e2cd9f'
     
+}else if(hora <= 18){
+    img.src = 'imagem/tarde.jpg'
+    document.body.style.background = '#b9846f'
+}else{
+    img.src = 'imagem/noite.jpg'
+    document.body.style.background = '#515154'
+}
 }
