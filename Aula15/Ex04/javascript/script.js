9c0cadf5bb2ae2eb0c40b6b6bf9222581d3491c0
Vianah-dev <67674592+Vianah-dev@users.noreply.github.com>
2020-08-31 18:53:04 -0300

    commit
    Exercicio da soma dos valores pares e impares realizada com sucesso.

--- Aula15/Ex04/javascript/script.js
+++ Aula15/Ex04/javascript/script.js
@@ -1,5 +1,27 @@
+function calcular(){
+    var txtnum = document.querySelector('input#txtnum')
+    var msg = document.querySelector('div#msg')
+    
+    if(txtnum.value.length == 0){
+        msg.innerHTML = ('Imposivel Calcular')
+        window.alert('[ERRO] Preencha o Campo!')
+    }else{
+        var num = Number(txtnum.value)
+        var soma = 0
 
-
-
-
+        if(num% 2 == 0){
+        for (var i = 0; i<= num; i+=2) { 
+            soma += i
+        }
+            var valor = 'par'
+        }else{
+        for (var i = 1; i<= num; i+=2) { 
+            soma += i
+        }
+            var valor = 'impar'
+        }
+        msg.innerHTML = `A soma dos valores ${valor} e ${soma}`
+    }
+    
+}
    
