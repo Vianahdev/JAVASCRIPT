517968dd9328023d5e745b6561085c10f0ff9c74
Matheus Viana <67674592+Vianahdev@users.noreply.github.com>
2023-05-30 01:19:36 -0300

    Delete Aula17 directory

--- Aula17/function04.js
+++
@@ -1,9 +0,0 @@
-function fatora(n) {
-    let fat = 1
-    for (let c = n; c > 1; c--) {
-        fat *= c  
-    }
-    return fat
-}
-
-console.log(fatora(5))
--- Aula17/function05.js
+++
@@ -1,9 +0,0 @@
-function fatora(n){
-    if(n == 1){
-        return 1
-    }else{
-        return n * fatora(n-1)
-    }
-}
-
-console.log(fatora(5))
