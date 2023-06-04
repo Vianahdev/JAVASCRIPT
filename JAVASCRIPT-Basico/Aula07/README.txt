0e97ea9ddcf393a6c484c089db8beb09162cc1bc
Matheus Viana <67674592+Vianahdev@users.noreply.github.com>
2023-06-04 18:38:24 -0300

    Add files via upload

---
+++ JAVASCRIPT-Basico/Aula07/README.txt
@@ -0,0 +1,12 @@
+###################################################################################################
+################################ AULA 07 | OPERADORES COMPARATIVOS ################################
+###################################################################################################
+
+== igual a, e compara o valor  
+=== valor igual e igual, e compara o valor e o tipo do dado
+!= não e igual
+!== não igual ou não igual
+> maior que
+< menor que
+>= maior ou igual a 
+<= menor ou igual a
---
+++ JAVASCRIPT-Basico/Aula07/script.js
@@ -0,0 +1,80 @@
+/*
+var a = 3;
+var b = 3;
+
+var result = (a == b);
+
+if (result == false){
+    console.log("ISSO E FALSO");
+}else{
+    console.log("ISSO E VERDADEIRO");
+} */
+
+/*
+var a = 3;
+var b = "3";
+
+var result = (a === b);
+
+if (result == false){
+    console.log("ISSO E FALSO");
+}else{
+    console.log("ISSO E VERDADEIRO");
+}
+*/
+
+/*
+var a = 3;
+var b = 3;
+
+var result = (a != b);
+
+if (result == false){
+    console.log("ISSO E NÃO E DIFERENTE");
+}else{
+    console.log("ISSO E DIFERENTE");
+}
+*/
+
+/*
+var a = 3;
+var b = "3";
+
+var result = (a !== b);
+
+if (result == false){
+    console.log("ISSO E NÃO E DIFERENTE");
+}else{
+    console.log("ISSO E DIFERENTE");
+}
+*/
+
+/*
+var a = 5;
+var b = 5;
+
+var result = (a > b);
+
+if( a === b){
+    console.log("MESMO VALOR")
+}else if (result == false){
+    console.log( a + " NÃO E MAIOR QUE " + b);
+}else{
+    console.log(a + " E MAIOR QUE " + b);
+}
+*/
+
+/*
+var a = 6;
+var b = 5;
+
+var result = (a < b);
+
+if( a === b){
+    console.log("MESMO VALOR")
+}else if (result == false){
+    console.log( a + " NÃO E MAIOR QUE " + b);
+}else{
+    console.log(a + " E MAIOR QUE " + b);
+}
+*/
