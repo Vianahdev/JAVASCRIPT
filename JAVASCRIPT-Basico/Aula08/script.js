abd804d8e713ad147c795e27a215d55678c6e303
Matheus Viana <67674592+Vianahdev@users.noreply.github.com>
2023-06-04 18:38:36 -0300

    Add files via upload

---
+++ JAVASCRIPT-Basico/Aula08/script.js
@@ -0,0 +1,38 @@
+/* && 
+ 
+var idade = 25;
+
+var maior20 = idade > 20;
+var menor30 = idade < 30;
+
+var entre = maior20 && menor30;
+
+console.log("Idade", idade)
+
+console.log("Maior que vinte", maior20);
+console.log("Menor que trinta", menor30);
+
+console.log("Entre 20 e 30", entre);
+*/
+
+
+
+/* ||
+
+var idade = 66;
+
+var menor10 = idade < 10;
+var maior65 = idade > 65;
+
+var entre = menor10 || maior65;
+
+if(entre == false){
+    console.log("VOCE NÃO TEM DIREITO A ENTRADA GRATUITA")
+}else{
+    console.log("VOCE TEM DIREITO A ENTRADA GRATUITA")
+}
+*/
+
+/* NEGATIVO
+console.log(!true)
+*/
