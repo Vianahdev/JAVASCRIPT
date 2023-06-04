6d7a8d7490832f4a15967378e859085a09aaad2c
Matheus Viana <67674592+Vianahdev@users.noreply.github.com>
2023-06-04 16:32:54 -0300

    Add files via upload

---
+++ JAVASCRIPT-Basico/Aula05/README.txt
@@ -0,0 +1,11 @@
+###################################################################################################
+############################### AULA 05 | OPERADORES ARITMETICOS ##################################
+###################################################################################################
+
++ ADIÇÃO
+- SUBTRAÇÃO
+* MULTIPLICAÇÃO
+/  DIVISÃO
+% MÓDULO  Tenta fazer uma divisão, retorna o resto da divisão.
+++ INCREMENTO  ex. a++ ira acrescentar um no valor total, dependendo da posição pode variar a operação.
+-- DECREMENTO ex. a-- subtrair um no valor total, dependendo da posição pode variar a operação.
---
+++ JAVASCRIPT-Basico/Aula05/script.js
@@ -0,0 +1,70 @@
+//ADIÇÃO
+
+var a = 10;
+var b = 15;
+
+var result_adi = a + b;
+
+console.log("ADIÇÃO " + result_adi); 
+
+
+ //SUBTRAÇÃO
+
+var a = 10;
+var b = 15;
+
+var result_sub = a - b;
+
+console.log("SUBTRAÇÃO " + result_sub); 
+
+
+//DIVISÃO
+
+var a = 10;
+var b = 15;
+
+var result_div = a / b;
+
+console.log("DIVISÃO " + result_div); 
+
+
+ //MULTIPLICAÇÃO
+
+var a = 10;
+var b = 15;
+
+var result_mult = a * b;
+
+console.log("MULTIPLICAÇÃO " + result_mult); 
+
+
+ //MODULO
+
+var a = 10;
+var b = 15;
+
+var result_mod = a % b;
+
+console.log("MODULO " + result_mod);
+
+
+
+//INCREMENTO
+
+var a = 10;
+
+a++;
+
+//*var result = **a;
+
+console.log("INCREMENTO " + a); 
+
+
+ //DECREMENTO
+var b = 10;
+
+b--
+
+//*var result = --a;
+
+console.log("DECREMENTO " + b);  
