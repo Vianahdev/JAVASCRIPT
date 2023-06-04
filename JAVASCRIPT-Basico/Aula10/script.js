1bf60378c8417207481ac5c6fccdd14d2a8e75eb
Matheus Viana <67674592+Vianahdev@users.noreply.github.com>
2023-06-04 18:38:58 -0300

    Add files via upload

---
+++ JAVASCRIPT-Basico/Aula10/script.js
@@ -0,0 +1,19 @@
+
+// PARSEINT
+
+var a = "3";
+var b = "6";
+
+var result = parseInt(a) + parseInt(b);
+
+console.log(result);
+
+
+// PARSEFLOAT
+
+var a = "3.7";
+var b = "6.3";
+
+var result = parseFloat(a) + parseFloat(b);
+
+console.log(result);
