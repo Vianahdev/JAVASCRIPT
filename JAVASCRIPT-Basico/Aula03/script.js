c3e2b22bbb4cf6fcd1d3f8dd21308232f65e82ec
Matheus Viana <67674592+Vianahdev@users.noreply.github.com>
2023-06-04 15:37:46 -0300

    Add files via upload

---
+++ JAVASCRIPT-Basico/Aula03/script.js
@@ -0,0 +1,12 @@
+var a = 6;
+var b = 6;
+var c = 6;
+
+var result = (a+b+c) / 3;
+
+if (result < 6){
+    console.log("Reprovado Média é: " + result);
+}else{
+    console.log("Aprovado Média é: " + result);
+}
+
