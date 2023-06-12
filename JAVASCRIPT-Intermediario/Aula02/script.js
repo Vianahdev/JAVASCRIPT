d41b927050cbd3a392cc5a46fe4c72ba2e53b8d1
Matheus Viana <67674592+Vianahdev@users.noreply.github.com>
2023-06-11 23:30:10 -0300

    Add files via upload

---
+++ JAVASCRIPT-Intermediario/Aula02/script.js
@@ -0,0 +1,12 @@
+let l_console = document.getElementById("paraf");
+console.log(l_console);
+
+let l_console1 = document.getElementsByClassName("paragrafo");
+console.log(l_console1);
+
+let l_console2  = document.getElementsByTagName("p")
+console.log(l_console2);
+
+// MODIFICANDO ELEMENTOS //
+
+l_console.innerHTML="";
