ff12655062bf6d7434ebfafb3d530463671f0207
Matheus Viana <matheus.viana.mv28@gmail.com>
2023-07-14 12:12:09 -0300

    Update filter.js

--- JAVASCRIPT-Avançado/filter.js
+++ JAVASCRIPT-Avançado/filter.js
@@ -1,8 +1,4 @@
 const ages = [37, 35, 45, 67, 15, 12, 11];
-const result = ages.filter(checkAdult);
-
-function checkAdult(age){
-  return age >= 35;
-}
+const result = ages.filter(age => age >= 37);
 
 document.write(result);
