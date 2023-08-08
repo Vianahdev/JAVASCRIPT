df081f908600135beb95cb435e23cd382bd3f07a
Matheus Viana <matheus.viana.mv28@gmail.com>
2023-08-08 17:16:04 -0300

    Update JAVASCRIPT-Dom
    Manipulating element size.

--- JAVASCRIPT-Dom/style_elements.js
+++ JAVASCRIPT-Dom/style_elements.js
@@ -1,5 +1,7 @@
 let l_areaColor = document.querySelector('div#area');
 
-function validaEstilo(color){
+function validaEstilo(color, w, h){
   l_areaColor.style.backgroundColor = color;
+  l_areaColor.style.width = w;
+  l_areaColor.style.height = h;
 }
