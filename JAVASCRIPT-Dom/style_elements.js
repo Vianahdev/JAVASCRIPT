da0c9dc71a1010705ff4aaa46fcdf0c5ca3658ab
Matheus Viana <matheus.viana.mv28@gmail.com>
2023-08-08 16:06:46 -0300

    Update JAVASCRIPT-Dom
    Update function.

--- JAVASCRIPT-Dom/style_elements.js
+++ JAVASCRIPT-Dom/style_elements.js
@@ -1,11 +1,5 @@
- let l_areaColor = document.querySelector('div#area');
+let l_areaColor = document.querySelector('div#area');
 
 function validaEstilo(color){
-  if(color == 'Red'){
-    l_areaColor.style.backgroundColor = 'Red';
-  }else if(color == 'Green'){
-    l_areaColor.style.backgroundColor = 'Green';
-  }else{
-    l_areaColor.style.backgroundColor = 'blue';
-  }
+  l_areaColor.style.backgroundColor = color;
 }
